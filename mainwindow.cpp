3bee25c39c39594a635a4ba0b3b7658e0dda13b7
MrKarimiD <mr.karimi.d@gmail.com>
2014-08-06 20:31:04 +0430

    changes

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -113,4 +113,6 @@ void MainWindow::cam_timeout()
     QImage imgIn= QImage((uchar*) outputFrame.data, outputFrame.cols, outputFrame.rows, outputFrame.step, QImage::Format_RGB888);
 
     ui->outputLabel->setPixmap(QPixmap::fromImage(imgIn));
+
+    ksdfjsudhfsdhf
 }
